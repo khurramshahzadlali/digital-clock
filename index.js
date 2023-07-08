13d6c3d3c23b803dab1f7e9f3ff52d6c02979762
Khurram Shahzad <106273270+khurramshahzadlali@users.noreply.github.com>
2023-07-08 22:27:25 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const hourEl = document.getElementById("hours");
-const minuteEl = document.getElementById("minutes");
-const secondEl = document.getElementById("seconds");
-const ampmEl = document.getElementById("ampm");
+// const hourEl = document.getElementById("hours");
+// const minuteEl = document.getElementById("minutes");
+// const secondEl = document.getElementById("seconds");
+// const ampmEl = document.getElementById("ampm");
 
 function updateClock() {
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
-  let ampm = "AM";
+//  let ampm = "AM";
 
-  if (h > 12) {
-    h = h - 12;
+  if (h = h - 12) { 
     ampm = "PM";
+  } else {
+    ampm = "AM"
   }
 
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
 
-  hourEl.innerText = h;
-  minuteEl.innerText = m;
-  secondEl.innerText = s;
-  ampmEl.innerText = ampm;
+  document.getElementById("hours").innerText = h;
+  document.getElementById("minutes").innerText = m;
+  document.getElementById("seconds").innerText = s;
+  document.getElementById("ampm").innerText = ampm;
 }
 
 setInterval(updateClock,1000);
